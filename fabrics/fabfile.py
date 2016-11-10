e9f3c00622daa0e1139b1082423cd36a99e96e33
thinksabin <think.sabin@gmail.com>
2016-11-10 13:23:35 +0545

    added platform detection and user listing for ubuntu and aws ami

--- fabrics/fabfile.py
+++ fabrics/fabfile.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 #Execute the script with the following command:
-#    fab -i /location/to/ssh/key--port 22 -H frog@subisu.lftechnology.com create
+#    fab -i /location/to/ssh/key--port 22 -H frog@domain.com create
 
 __author__ = 'thinksabin'
 
 import getpass
 import os
 import socket
+import subprocess
 
 from fabric.api import *
 
@@ -23,30 +24,62 @@ key_dir = home_dir +"/keys/"
 #local_user = "sabin" # local user of the workstation from which this fabfile is run
 local_user = getpass.getuser()
 
+def list_user():
+    try:
+        print("###### Finding the system Platform ######")
+        with hide('output'):
+            os_type = run("python -mplatform")
+            os_type = str(os_type)
+            print("###### %s ######") %os_type
+            os_l = os_type.split("-")
+            
+            plaform = "Ubuntu"
+
+            if plaform in os_l:
+                print("")
+                print("###### OS detected Ubuntu. Listing users above uid => 1000. ###### ")
+                with hide('output'):
+                    users = run("awk  -F':' '$3>999 {print $1 }' /etc/passwd | column -t | grep -v nobody")
+                    print("")
+                    print(users)
+            else:
+                print("")
+                print("###### OS detected not Ubuntu. Listing users above uid =>500. ######")
+                with hide('output'):
+                    users = run("awk  -F':' '$3>499 {print $1 }' /etc/passwd | column -t | grep -v nobody")
+                    print("")
+                    print(users)
+
+    except:
+        print ("###### Couldnt run system check ######")
+
+
 
 def create():
     # username used to ssh to the remote machine
     ssh_user = prompt("Enter Username to ssh into the remote machine (eg:root, ubuntu, ec2-user):")
+    list_user()
+
     choice = ""
     while choice != "NO":
-        print "\n\n-------------  Creating user on remote machine %s  -------------\n"
+        print ("\n\n-------------  Creating user on remote machine %s  -------------\n")
         username = prompt("Enter Username to create:")
 
-        print username
+        print (username)
         try:
             sudo("adduser %s" %username)
-            print "\n------------- User created  -------------\n"
+            print ("\n------------- User created  -------------\n")
         except:
-            print "\n------------- User already exists  -------------\n"
+            print ("\n------------- User already exists  -------------\n")
             pass
-        print "\n------------- Creating Keys -------------\n"
+        print ("\n------------- Creating Keys -------------\n")
         with cd("/home/%s" %username):
             sudo("ssh-keygen -b 1024 -f %s -t dsa" %username)
             try:
                 sudo("mkdir .ssh")
             except:
                 pass
-            print "\n------------- Keys created  -------------\n"
+            print ("\n------------- Keys created  -------------\n")
             sudo("chmod 700 .ssh")
             sudo("cat %s.pub >> .ssh/authorized_keys" %username)
             sudo("mv %s %s.pem" %(username,username) )
@@ -74,12 +107,12 @@ def create():
             else:
                 print("No sudo access given to %s" %username)
 
-        print "\n------------- Downloading Keys -------------\n"
+        print ("\n------------- Downloading Keys -------------\n")
         get("/home/Keys/%s.*" %username,"/tmp")
 
         local("chown %s /tmp/%s.* " %(local_user,username))
         local("mv /tmp/%s.* %s" %(username,key_dir))
-        print "\n\n------------- Key files downloaded to %s   -------------\n" %key_dir
+        print ("\n\n------------- Key files downloaded to %s   -------------\n") %key_dir
 
         choice = prompt("Do you want to create another user (Y/n):")
         if choice == "N" or choice =="n" or choice == "no" or choice== "No":
@@ -87,4 +120,4 @@ def create():
         else:
             continue
 
-    print "\n------------- Thank you  -------------\n"
+    print ("\n------------- Thank you  -------------\n")
