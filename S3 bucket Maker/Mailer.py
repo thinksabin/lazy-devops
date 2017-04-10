7beb63bf39ac6b3663879236eb159bfaedc5a24b
Sabin Ranjit <think.sabin@gmail.com>
2017-04-11 00:33:01 +0545

    Update Mailer.py

--- S3 bucket Maker/Mailer.py
+++ S3 bucket Maker/Mailer.py
@@ -1,4 +1,4 @@
-__author__ = 'gambit'
+
 
 import datetime
 import time
@@ -10,13 +10,13 @@ from email.mime.multipart import MIMEMultipart
 from smtplib import SMTP
 
 class Mailer():
-    # credentials for the user Lf-sandbox-SES-generic-user in the LF sandbox AWS account
-    smtp_server = 'email-smtp.us-east-1.amazonaws.com'
-    smtp_username = 'AKIAJD4PYHW2WP5F73YA'
-    smtp_password = 'AmornLLC5nHbpYojXnw/WSudvOtRGc/FG/MCrlv9B+GL'
+   
+    smtp_server = ''
+    smtp_username = ''
+    smtp_password = ''
     smtp_port = '587'
 
-    from_add = "noreply@lftechnology.com"
+    from_add = "noreply@example.com"
 
     def __init__(self, receiver, subject, body, filepath, filename):
         self.receiver = receiver
@@ -44,6 +44,6 @@ class Mailer():
         mail.starttls()
         mail.ehlo()
         mail.login(self.smtp_username,self.smtp_password)
-        #mail.login('prabinsubedi@lftechnology.com', '%s' % password)
+   
         mail.sendmail(self.from_add, self.receiver, self.msg.as_string())
         mail.quit()
